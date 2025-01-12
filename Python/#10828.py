e19441009b01c82b5ccb8b8e669f64d91f4df84e
yunadada <ynson0216@naver.com>
2025-01-12 12:36:12 +0900

    스택

---
+++ Python/#10828.py
@@ -0,0 +1,25 @@
+import sys 
+
+N = int(input())
+stack = []
+for _ in range(N):
+    command = sys.stdin.readline().rstrip().split()
+    if (command[0] == 'push'):
+        stack.append(command[1])
+    elif(command[0] == 'pop'):
+        if(len(stack) == 0):
+            print(-1)
+        else:
+            print(stack.pop())
+    elif(command[0] == 'size'):
+        print(len(stack))
+    elif(command[0] == 'empty'):
+        if(len(stack) == 0):
+            print(1)
+        else:
+            print(0)
+    else:
+        if(len(stack) == 0):
+            print(-1)
+        else:
+            print(stack[-1])
