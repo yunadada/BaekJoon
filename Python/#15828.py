04ccdc2afb716bba4d0b7d76db1c3ea08467a011
yunadada <ynson0216@naver.com>
2025-01-21 10:06:51 +0900

    Router

--- Python/#15828.py
+++ Python/#15828.py
@@ -16,6 +16,32 @@ if(router):
     print(' '.join(map(str, router)))
 else:
     print('empty')
-    
 
+# 더 효율적인 풀이 (접근 방법은 비슷하나, 수행 시간이나 메모리 측면에서 훨씬 효율적)
+# import sys
+# from collections import deque
 
+# input = sys.stdin.readline
+
+# def solution():
+#     n = int(input())
+
+#     buffer = deque()
+#     while True:
+#         packet = input().strip()
+#         if packet == "-1":
+#             break
+#         elif packet == "0":
+#             buffer.popleft()
+#         elif len(buffer) == n:
+#             continue
+#         else:
+#             buffer.append(packet)
+
+#     if not buffer:
+#         print("empty")
+#     else:
+#         print(" ".join(buffer))
+
+# if __name__ == "__main__":
+#     solution()
