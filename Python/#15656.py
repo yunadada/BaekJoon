8ef6f3383d1472aa48e47e4559d2495c309203e0
yunadada <ynson0216@naver.com>
2025-02-24 15:27:10 +0900

    N과 M(7)

--- Python/#15656.py
+++ Python/#15656.py
@@ -1,18 +1,18 @@
-def DFS(num):
+def DFS():
     global M
     if (len(sequence) == M):
         print(*sequence)
         return
     for i in range(N):
         sequence.append(SEQ[i])
-        DFS(i)
+        DFS()
         sequence.pop()
 
 N, M = map(int, input().split())
 SEQ = list(map(int, input().split()))
 SEQ.sort()
 sequence=[]
-DFS(SEQ[0])
+DFS()
 
 # 더 효율적인 풀이(라이브러리 사용)
 # from itertools import product
